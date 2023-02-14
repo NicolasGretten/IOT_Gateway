09c94bfd88a61c9b0de98e63629d1cc0dc5cd128
nicolas gretten <ngretten@gmail.com>
2023-02-14 23:16:55 +0100

    upload debug

--- app/Traits/MicroserviceTrait.php
+++ app/Traits/MicroserviceTrait.php
@@ -13,20 +13,31 @@ trait MicroserviceTrait
     public function uri(Request $request, String $uri): JsonResponse
     {
         try{
-            $response = match ($request->method()) {
-                'POST' => Http::withHeaders([
-                    'Authorization' => $request->header('Authorization') ?? null,
-                ])->post($uri . $request->getRequestUri(), $request->request->all())->body(),
-                'GET' => Http::withHeaders([
-                    'Authorization' => $request->header('Authorization') ?? null,
-                ])->get($uri . $request->getRequestUri(), $request->request->all())->body(),
-                'DELETE' => Http::withHeaders([
-                    'Authorization' => $request->header('Authorization') ?? null,
-                ])->delete($uri . $request->getRequestUri(), $request->request->all())->body(),
-                'PATCH' => Http::withHeaders([
-                    'Authorization' => $request->header('Authorization') ?? null,
-                ])->patch($uri . $request->getRequestUri(), $request->request->all())->body(),
-            };
+            if($uri === env('IMAGE_API') && $request->hasFile('file') && $request->file('file')->isValid()){
+                $image = $request->file('file');
+                $response = Http::attach('file', file_get_contents($image), 'image.png')
+                    ->withHeaders([
+                        'Authorization' => $request->header('Authorization') ?? null,
+                    ])
+                    ->post($uri . $request->getRequestUri(), $request->request->all())->body();
+
+            } else {
+                $response = match ($request->method()) {
+                    'POST' => Http::withHeaders([
+                        'Authorization' => $request->header('Authorization') ?? null,
+                    ])->post($uri . $request->getRequestUri(), $request->request->all())->body(),
+                    'GET' => Http::withHeaders([
+                        'Authorization' => $request->header('Authorization') ?? null,
+                    ])->get($uri . $request->getRequestUri(), $request->request->all())->body(),
+                    'DELETE' => Http::withHeaders([
+                        'Authorization' => $request->header('Authorization') ?? null,
+                    ])->delete($uri . $request->getRequestUri(), $request->request->all())->body(),
+                    'PATCH' => Http::withHeaders([
+                        'Authorization' => $request->header('Authorization') ?? null,
+                    ])->patch($uri . $request->getRequestUri(), $request->request->all())->body(),
+                };
+            }
+
             $response = json_decode($response);
             return response()->json($response->content->body);
         } catch (Exception $e) {
