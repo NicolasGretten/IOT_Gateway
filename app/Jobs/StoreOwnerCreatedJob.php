9f52a5edb7f8b75e92278659adb82f2631042c85
nicolas gretten <ngretten@gmail.com>
2023-02-15 21:19:00 +0100

    docs

--- app/Jobs/StoreOwnerCreatedJob.php
+++ app/Jobs/StoreOwnerCreatedJob.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class OwnerCreatedJob implements ShouldQueue
+class StoreOwnerCreatedJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
     private $data;
