6edc623abcad326e01e547d698f2643251576f8b
nicolas gretten <ngretten@gmail.com>
2023-02-26 09:07:52 +0100

    subscription

--- app/Http/Controllers/Payment/WebhookController.php
+++ app/Http/Controllers/Payment/WebhookController.php
@@ -5,11 +5,10 @@ namespace App\Http\Controllers\Payment;
 use App\Traits\MicroserviceTrait;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class WebhookController
 {
-    use MicroserviceTrait;
-
     /**
      * Handle a Stripe webhook call.
      *
@@ -18,6 +17,35 @@ class WebhookController
      */
     public function handleWebhook(Request $request): JsonResponse
     {
-        return $this->uri($request, env("PAYMENT_API"));
+        if(!empty($request)) {
+            $payload = json_decode(json_decode($request->getContent(), true), true);
+            $method = 'handle'.Str::studly(str_replace('.', '_', $payload['type']));
+
+            if (method_exists($this, $method)) {
+                return $this->{$method}($payload);
+            }
+
+            return $this->webhookError(['error' => $payload['type'] . ' ('. $method .') method not found.']);
+        }
+
+        return $this->webhookError(['error' => 'Request empty.']);
+    }
+
+    public function webhookSuccess(array $webhook): JsonResponse
+    {
+        return response()->json([
+            'status' => 'SUCCESS',
+            'webhook' => [
+                'id' => $webhook['id']
+            ]
+        ], 201);
+    }
+
+    public function webhookError(array $webhook): JsonResponse
+    {
+        return response()->json([
+            'status' => 'FAILURE',
+            'error' => $webhook['error']
+        ], 409);
     }
 }
